99864548c6a9856d4ab75a8da7bb56384c2f3eb9
mn <monikanoun2009@gmail.com>
2026-05-26 14:54:20 -0700

    Improved buttons, added customization options

--- menu.gd
+++ menu.gd
@@ -16,6 +16,7 @@ func _on_texture_rect_mouse_entered() -> void:
 
 
 func _on_axiom_button_pressed() -> void:
+	clearDesc()
 	clearSuits()
 	$descriptionPopUp.visible = true
 	$descriptionPopUp/Description/Button/Axiom.visible = true
@@ -23,18 +24,23 @@ func _on_axiom_button_pressed() -> void:
 	$Axiom.visible = true
 
 func _on_poly_button_pressed() -> void:
+	clearDesc()
 	clearSuits()
 	$descriptionPopUp.visible = true
+	$descriptionPopUp/Description/Button/Poly.visible = true
 	$descriptionPopUp/AnimationPlayer.play("entering")
 	$Poly.visible = true
 
 func _on_orange_button_pressed() -> void:
+	clearDesc()
 	clearSuits()
 	$descriptionPopUp.visible = true
+	$descriptionPopUp/Description/Button/Orange.visible = true
 	$descriptionPopUp/AnimationPlayer.play("entering")
 	$Orange.visible = true
 
 func _on_space_x_button_pressed() -> void:
+	clearDesc()
 	clearSuits()
 	$descriptionPopUp.visible = true
 	$descriptionPopUp/Description/Button/SpaceX.visible = true
@@ -42,7 +48,6 @@ func _on_space_x_button_pressed() -> void:
 	$SpaceX.visible = true
 
 func _on_button_pressed() -> void:
-	
 	$descriptionPopUp.visible = false
 
 func clearSuits():
@@ -50,6 +55,12 @@ func clearSuits():
 	$Poly.visible = false
 	$Orange.visible = false
 	$SpaceX.visible = false
+	
+func clearDesc():
+	$descriptionPopUp/Description/Button/Poly.visible = false
+	$descriptionPopUp/Description/Button/Orange.visible = false
+	$descriptionPopUp/Description/Button/Axiom.visible = false
+	$descriptionPopUp/Description/Button/SpaceX.visible = false
 
 
 func _on_q_but_pressed() -> void:
@@ -58,3 +69,137 @@ func _on_q_but_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	$"q&a".visible = false
+
+
+
+func _on_customize_pressed() -> void:
+	$ShopBody.visible = false
+	$ShopButton.scale = Vector2(.8,.8)
+	$ShopButton.position = Vector2(632, 88)
+	$Customize.scale = Vector2(1,1)
+	$Customize.position = Vector2(816, 80)
+	$CustomizeBody.visible = true
+
+func _on_shop_button_pressed() -> void:
+	$ShopBody.visible = true
+	$ShopButton.scale = Vector2(1,1)
+	$ShopButton.position = Vector2(608, 80)
+	$Customize.scale = Vector2(.8,.8)
+	$Customize.position = Vector2(816, 88)
+	$CustomizeBody.visible = false
+
+func _on_nobrows_pressed() -> void:
+	print("noBrows")
+	$CustomizeBody/MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/Eyebrows2/Panel6
+
+func _on_no_eb_pressed() -> void:
+	$B.visible = false 
+	$B2.visible = false 
+	$B3.visible = false 
+	$B4.visible = false 
+	$B5.visible = false
+
+
+func _on_eb_1_pressed() -> void:
+	$B.visible = false 
+	$B2.visible = false 
+	$B3.visible = false 
+	$B4.visible = false 
+	$B5.visible = false
+	$B.visible = true 
+
+func _on_eb_2_pressed() -> void:
+	$B.visible = false 
+	$B2.visible = false 
+	$B3.visible = false 
+	$B4.visible = false 
+	$B5.visible = false
+	$B2.visible = true 
+
+func _on_eb_3_pressed() -> void:
+	$B.visible = false 
+	$B2.visible = false 
+	$B3.visible = false 
+	$B4.visible = false 
+	$B5.visible = false
+	$B3.visible = true 
+
+func _on_eb_4_pressed() -> void:
+	$B.visible = false 
+	$B2.visible = false 
+	$B3.visible = false 
+	$B4.visible = false 
+	$B5.visible = false
+	$B4.visible = true 
+
+func _on_eb_5_pressed() -> void:
+	$B.visible = false 
+	$B2.visible = false 
+	$B3.visible = false 
+	$B4.visible = false 
+	$B5.visible = false
+	$B5.visible = true 
+
+func clearEyes(): 
+	$E.visible = false 
+	$E2.visible = false 
+	$E3.visible = false 
+	$E4.visible = false 
+	$E5.visible = false 
+	$E6.visible = false
+	
+func _on_eyes_pressed() -> void:
+	clearEyes()
+	$E.visible = true 
+
+func _on_eyes_2_pressed() -> void:
+	clearEyes()
+	$E2.visible = true 
+
+func _on_eyes_3_pressed() -> void:
+	clearEyes()
+	$E3.visible = true 
+
+func _on_eyes_4_pressed() -> void:
+	clearEyes()
+	$E4.visible = true 
+
+func _on_eyes_5_pressed() -> void:
+	clearEyes()
+	$E5.visible = true 
+	
+func _on_eyes_6_pressed() -> void:
+	clearEyes()
+	$E6.visible = true 
+
+func mouthClear():
+	$M.visible = false 
+	$M2.visible = false 
+	$M3.visible = false 
+	$M4.visible = false 
+	$M5.visible = false 
+	$M6.visible = false
+
+func _on_mouth_pressed() -> void:
+	mouthClear()
+	$M.visible = true 
+
+func _on_mouth_2_pressed() -> void:
+	mouthClear()
+	$M2.visible = true 
+
+func _on_mouth_3_pressed() -> void:
+	mouthClear()
+	$M3.visible = true 
+
+func _on_mouth_4_pressed() -> void:
+	mouthClear()
+	$M4.visible = true 
+	
+func _on_mouth_5_pressed() -> void:
+	mouthClear()
+	$M5.visible = true 
+	
+func _on_mouth_6_pressed() -> void:
+	mouthClear()
+	$M6.visible = true 
